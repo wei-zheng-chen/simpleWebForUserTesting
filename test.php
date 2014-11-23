c907f6b4c8fdd4c04c0599e4b9085089ad63f472
wei-zheng-chen <wei.zheng.chen@duke.edu>
2014-11-23 00:54:36 -0500

    testin

---
+++ test.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html >
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="style.css" />
+
+<title>Suggestions</title>
+</head>
+
+<body>
+    <div id="page">
+
+        <div id="header">
+        	<h1>Health Alert</h1>  
+        </div>
+
+        <div id="bar">
+            <ul>
+                <li><a href="index.html">Home </a></li>
+                <li><a href="FinishedFeatures.html">Finished Features </a></li>
+                <li><a href="UnfinishedTask.html">Unfinished Tasks </a></li>
+                <li><a href="SurveyApp.html">Survey and App</a></li>
+                <li><a href="Instructions.html"> Instructions</a></li>
+            </ul>
+        </div>
+        <div class="contentTitle"><h1>Give Us Suggestions!</h1></div>
+        <div class="contentText">
+            <p> 
+                If you have addition ideas or features or even bad criticism about the application that you want us to work on, feel free to type out your desires on the box below and we'll get to it. 
+             </p>
+            <form name="suggestionForm" action="submitButton.php" method="post">
+                    <b> Type you Suggestions Below: <b/>
+                    <br><br/>
+                    <textarea name="suggestionBox" maxlength="2800" rows="10" cols="104" placeholder="Start typing here"></textarea> 
+                    <br><br/>
+                    <input type="submit" value="Submit" />
+            </form>
+        </div>
+
+            <br></br>
+            <br></br>
+
+ 	</div>
+
+</body>
+</html>
