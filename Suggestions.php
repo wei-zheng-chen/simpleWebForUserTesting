adbe25eac17016ade35f24de1cc21717434a8324
wei-zheng-chen <wei.zheng.chen@duke.edu>
2014-11-23 01:20:38 -0500

    combine php n html

--- Suggestions.php
+++ Suggestions.php
@@ -25,7 +25,47 @@
         <div class="contentText">
             <p> 
                 If you have addition ideas or features or even bad criticism about the application that you want us to work on, feel free to type out your desires on the box below and we'll get to it. 
-             </p>
+            </p>
+
+            <h3>Here Are The Suggestions<h3> 
+            <?php
+                    // Create connection
+                    $con=mysqli_connect("localhost","root","bitnami","user_testing");
+
+                    // Check connection
+                    if (mysqli_connect_errno()){
+                        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+                    }
+
+                    $sql = "SELECT suggestion 
+                            FROM suggestions"
+
+                    // Check if there are results
+                    if ($result = mysqli_query($con, $sql)){
+                        // If so, then create a results array and a temporary one
+                        // to hold the data
+                        $resultArray = array();
+                        $tempArray = array();
+                        // Loop through each row in the result set
+                        while($row = $result->fetch_object()){
+                            // Add each row into our results array
+                            $tempArray = $row;
+                            array_push($resultArray, $tempArray);
+                        }
+                    }
+                    mysqli_close($con);
+            ?>
+            <ul>
+            <?php
+                    foreach $resultArray as $value{ 
+            ?>
+                    <li> <?=value?> </li>
+
+
+            <?php
+                    }
+            ?>
+
 
 
             <?php
@@ -38,14 +78,11 @@
                         echo "Failed to connect to MySQL: " . mysqli_connect_error();
                     }
 
-                    echo $_POST['suggestionBox'];
-
                     $sql = "INSERT INTO suggestions (suggestion) 
                             VALUES ('".nl2br($_POST['suggestionBox'])."')";
 
 
                     if ($con->query($sql) == TRUE) {
-                        echo "New record created successfully";
                     } else {
                         echo "Error: " . $sql . "<br>" . $con->error;
                     }
