3b0795b3836b324c89c7022113e09841cca3f094
wei-zheng-chen <wei.zheng.chen@duke.edu>
2014-11-23 01:00:59 -0500

    rewrote suggestion

--- Suggestions.php
+++ Suggestions.php
@@ -1,5 +1,3 @@
-<!DOCTYPE html >
-
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" type="text/css" href="style.css" />
@@ -28,13 +26,42 @@
             <p> 
                 If you have addition ideas or features or even bad criticism about the application that you want us to work on, feel free to type out your desires on the box below and we'll get to it. 
              </p>
+
+
+            <?php
+                if(isset($_POST['BtnSubmit'])){
+                    // Create connection
+                    $con=mysqli_connect("localhost","root","bitnami","user_testing");
+
+                    // Check connection
+                    if (mysqli_connect_errno()){
+                        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+                    }
+
+                    echo $_POST['suggestionBox'];
+
+                    $sql = "INSERT INTO suggestions (suggestion) 
+                            VALUES ('".nl2br($_POST['suggestionBox'])."')";
+
+
+                    if ($con->query($sql) == TRUE) {
+                        echo "New record created successfully";
+                    } else {
+                        echo "Error: " . $sql . "<br>" . $con->error;
+                    }
+
+                    mysqli_close($con);
+                }
+            ?>
+
             <form name="suggestionForm" action="submitButton.php" method="post">
                     <b> Type you Suggestions Below: <b/>
                     <br><br/>
                     <textarea name="suggestionBox" maxlength="2800" rows="10" cols="104" placeholder="Start typing here"></textarea> 
                     <br><br/>
-                    <input type="submit" value="Submit" />
+                    <input name="BtnSubmit" type="submit" value="Submit" />
             </form>
+            
         </div>
 
             <br></br>
