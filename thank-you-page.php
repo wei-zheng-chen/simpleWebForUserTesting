75dae9adf7414e3ef37de38e6ea12af288ec78e7
wei-zheng-chen <wei.zheng.chen@duke.edu>
2014-11-23 02:20:18 -0500

    modify thank you page

--- thank-you-page.php
+++ thank-you-page.php
@@ -1,5 +1,35 @@
-<html>
-	<p> thank you </p>
-	<a href="Suggestions.php"> GO BACK </a>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="style.css" />
 
+<title>Health Alert User Testing</title>
+</head>
+
+<body>
+    <div id="page">
+		
+        <div id="header">
+        	<h1>Health Alert</h1>  
+        </div>
+
+        <div id="bar">
+            <ul>
+            	<li><a href="index.html"> Home </a></li>
+                <li><a href="FinishedFeatures.html">Finished Features </a></li>
+                <li><a href="UnfinishedTask.html">Unfinished Tasks </a></li>
+                <li><a href="SurveyApp.html">Survey and App</a></li>
+                <li><a href="Instructions.html"> Instructions</a></li>
+            </ul>
+        </div>
+
+
+        <div class="contentTitle"><h1>Thank you for providing us feedback on our application!</h1></div>
+        <a href="Suggestions.php"> <h1> Click here to return back to suggestions or click the navigation bar to go to another page</a>
+ 
+        </div>
+        <br></br>
+        <br></br>
+
+</div>
+</body>
 </html>
